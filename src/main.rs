56995a038fc30376eba0bc3720f826572e41a206
yashthussu <yashuthussu@gmail.com>
2025-07-06 02:43:30 +0530

    opening file and handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let file=File::open("C:\\Users\\YASH\\Desktop\\coding\\Rust\\story.txt");
+    let result=match file{
+        Ok(success_file)=>success_file,
+        Err(error)=>{
+            eprintln!("Something went wrong reading the file:{error:?}");
+            process::exit(1);
+        }
+    };
+
+    println!("Result is :{result:#?}");
+    
 }
