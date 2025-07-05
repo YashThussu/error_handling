2500bcf812925f40f8476da2c456436b16ff92fa
yashthussu <yashuthussu@gmail.com>
2025-07-06 03:09:42 +0530

    reading path from user

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::fs::File;
 use std::process;
+use std::io::stdin;
 
 fn main() {
     
-    let file=File::open("C:\\Users\\YASH\\Desktop\\coding\\Rust\\story.txt");
+    println!("Please enter tha path of the file:");
+    let mut input=String::new();
+
+    let file_path_result=stdin().read_line(&mut input);
+
+    if let Err(error)= file_path_result{
+        eprintln!("Somthing went wrong the error msg is {error:?}");
+        process::exit(1);
+    };
+
+    let file=File::open(&input.trim());
     let result=match file{
         Ok(success_file)=>success_file,
         Err(error)=>{
