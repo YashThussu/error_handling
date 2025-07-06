0eaf0160032600bc4ee514038d539c1dc3bb3814
yashthussu <yashuthussu@gmail.com>
2025-07-07 02:15:55 +0530

    reading file contents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::process;
-use std::io::stdin;
+use std::io::{stdin,Read};
 
 fn main() {
     
@@ -14,15 +14,26 @@ fn main() {
         process::exit(1);
     };
 
-    let file=File::open(&input.trim());
-    let result=match file{
+    let  file=File::open(&input.trim());
+    let mut result=match file{
         Ok(success_file)=>success_file,
         Err(error)=>{
             eprintln!("Something went wrong reading the file:{error:?}");
             process::exit(1);
         }
-    };
+    }; // need to make this mutable for read trait to work
 
-    println!("Result is :{result:#?}");
+    let mut file_contents=String::new();
+    let read_operation=result.read_to_string(&mut file_contents); // read to string becomes available only after Read trait is brought in
+
+    if let Err(error)= read_operation{
+        eprintln!("Someething went wrong reading the string The error was {error:?}");
+        process::exit(1);
+    }
     
+    println!("Result is :{result:#?}");
+
+    println!("File Content: {file_contents}")
+    
+
 }
