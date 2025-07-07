d0bd6c72db8ddc51ce98fcf2520c33614eec1ec7
yashthussu <yashuthussu@gmail.com>
2025-07-08 01:36:54 +0530

    error propagation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use std::fs::File;
-use std::process;
-use std::io::{stdin,Read};
+use std::io::{self,stdin,Read};
 
 fn main() {
     
-    println!("Please enter tha path of the file:");
+   let file_result=read_file();
+
+   match file_result {
+
+    Ok(content)=>println!("{content}"),
+    Err(error)=>eprintln!("Error encountered:{error:?}")
+
+   };
+    
+
+}
+
+fn read_file()->Result<String,io::Error>{
+
+     println!("Please enter tha path of the file:");
     let mut input=String::new();
 
     let file_path_result=stdin().read_line(&mut input);
 
     if let Err(error)= file_path_result{
-        eprintln!("Somthing went wrong the error msg is {error:?}");
-        process::exit(1);
+        return Result::Err(error); // return the error here 
     };
 
     let  file=File::open(&input.trim());
     let mut result=match file{
         Ok(success_file)=>success_file,
         Err(error)=>{
-            eprintln!("Something went wrong reading the file:{error:?}");
-            process::exit(1);
+            return Err(error);
         }
     }; // need to make this mutable for read trait to work
 
@@ -27,13 +38,8 @@ fn main() {
     let read_operation=result.read_to_string(&mut file_contents); // read to string becomes available only after Read trait is brought in
 
     if let Err(error)= read_operation{
-        eprintln!("Someething went wrong reading the string The error was {error:?}");
-        process::exit(1);
+        return Err(error);
     }
     
-    println!("Result is :{result:#?}");
-
-    println!("File Content: {file_contents}")
-    
-
+    Ok(file_contents)
 }
