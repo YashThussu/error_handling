2d872d466fe052b37a9a4c51acb0de800618883c
yashthussu <yashuthussu@gmail.com>
2025-07-09 00:46:48 +0530

    using only fs module to read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs;
 use std::io::{self,stdin,Read};
 
 fn main() {
@@ -22,12 +22,5 @@ fn read_file()->Result<String,io::Error>{
 
     stdin().read_line(&mut input)?; // this terminates the code if it produces an error
 
-    let  mut file=File::open(&input.trim())?;
-
-    let mut file_contents=String::new();
-    
-    file.read_to_string(&mut file_contents)?;
-
-    
-    Ok(file_contents)
+    fs::read_to_string(input.trim())
 }
