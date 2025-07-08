bfae4db17eb1b9dfb4808397795939d4fd2a9826
yashthussu <yashuthussu@gmail.com>
2025-07-09 00:37:30 +0530

    error propagation symbol

--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,14 @@ fn read_file()->Result<String,io::Error>{
      println!("Please enter tha path of the file:");
     let mut input=String::new();
 
-    let file_path_result=stdin().read_line(&mut input);
+    stdin().read_line(&mut input)?; // this terminates the code if it produces an error
 
-    if let Err(error)= file_path_result{
-        return Result::Err(error); // return the error here 
-    };
-
-    let  file=File::open(&input.trim());
-    let mut result=match file{
-        Ok(success_file)=>success_file,
-        Err(error)=>{
-            return Err(error);
-        }
-    }; // need to make this mutable for read trait to work
+    let  mut file=File::open(&input.trim())?;
 
     let mut file_contents=String::new();
-    let read_operation=result.read_to_string(&mut file_contents); // read to string becomes available only after Read trait is brought in
+    
+    file.read_to_string(&mut file_contents)?;
 
-    if let Err(error)= read_operation{
-        return Err(error);
-    }
     
     Ok(file_contents)
 }
